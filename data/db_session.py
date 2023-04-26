035137d3e1bf5834db9aa9ff23cb36106b2ca159
verikirill <118463466+verikirill@users.noreply.github.com>
2023-04-27 01:12:35 +0400

    Add files via upload

--- data/db_session.py
+++ data/db_session.py
@@ -2,6 +2,7 @@ import sqlalchemy as sa
 import sqlalchemy.orm as orm
 from sqlalchemy.orm import Session
 import sqlalchemy.ext.declarative as dec
+
 SqlAlchemyBase = dec.declarative_base()
 
 __factory = None
@@ -21,10 +22,8 @@ def global_init(db_file):
 
     engine = sa.create_engine(conn_str, echo=False)
     __factory = orm.sessionmaker(bind=engine)
-
-    from . import __all_models
-
     SqlAlchemyBase.metadata.create_all(engine)
+
 
 def create_session() -> Session:
     global __factory
